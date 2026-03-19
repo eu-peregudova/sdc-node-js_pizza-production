7a3e3afbc576ac42fc2cb45b951de846862e1e98
Evgeniia <evgeniia.peregudova@stud.esdc.lt>
2026-03-19 18:58:34 +0200

    init: initialization

---
+++ eslint.config.ts
@@ -0,0 +1,39 @@
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-plugin-prettier/recommended';
+
+export default [
+    {
+        ignores: ['node_modules', 'dist', 'coverage'],
+    },
+    js.configs.recommended,
+    ...tseslint.configs.recommended,
+    prettier,
+    {
+        languageOptions: {
+            parserOptions: {
+                ecmaVersion: 2023,
+                sourceType: 'module',
+            },
+        },
+        rules: {
+            'prettier/prettier': 'error',
+            '@typescript-eslint/no-unused-vars': [
+                'error',
+                {
+                    argsIgnorePattern: '^_',
+                },
+            ],
+            'no-console': [
+                'warn',
+                {
+                    allow: ['warn', 'error', 'info'],
+                },
+            ],
+            eqeqeq: ['error', 'always'],
+            'no-implicit-coercion': 'error',
+            'prefer-const': 'error',
+            'no-var': 'error',
+        },
+    },
+];
